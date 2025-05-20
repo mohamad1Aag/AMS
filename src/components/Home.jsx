ec04d542d17b6db71d483ef8839d0807d10d9e76
Mohammad <aagmohamad4@gmail.com>
2025-05-20 11:14:53 +0300

    Initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,26 @@
 // src/components/Home.jsx
+import React, { useState } from "react";
 import './Home.css';
 
 function Home() {
+  const [navVisible, setNavVisible] = useState(false);
+
+  const toggleNav = () => {
+    setNavVisible(!navVisible);
+  };
+
   return (
     <div className="main">
       <header className="header">
         <div className="logo"></div>
-        <nav className="nav">
+        <button className="menu-btn" onClick={toggleNav} >
+        <svg width="30" height="30" viewBox="0 0 100 80" fill="#00b4db">
+    <rect width="100" height="15"></rect>
+    <rect y="30" width="100" height="15"></rect>
+    <rect y="60" width="100" height="15"></rect>
+  </svg> {/* أيقونة الهامبرغر */}
+        </button>
+        <nav className={`nav ${navVisible ? "active" : ""}`}>
           <a href="#">logo</a>
           <a href="#">Home</a>
           <a href="#">Services</a>
